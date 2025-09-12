a262c8b5cc660f47caf170bfbafbb7c38ba766ee
enzooog <enzo.gruel35@gmail.com>
2025-09-12 11:05:45 +0200

    initialise player

--- player.go
+++ player.go
@@ -1,10 +1,13 @@
 package player
 
-import "fmt" 
+import (
+	"fmt"
+	"time"
+)
 
 func player() {
 	fmt.Print(" Welcome to Zelda")
-
+	time.Sleep(8 * time.Second)
 	fmt.Print(" Choose one breed from these three")
 
 }
