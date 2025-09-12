bb04d80d4d6ace6e96bdc3d9306d5453f5a5de2e
allan paris <allanparis8@gmail.com>
2025-09-12 15:40:38 +0200

    initialisation menu

--- menu/menu.go
+++ menu/menu.go
@@ -5,40 +5,36 @@ import (
 	"os"
 )
 
-func menu() {
-	var key int
-	fmt.Scanf("%d", &key)
+func Show() {
+	for {
+		fmt.Println(" MENU ")
+		fmt.Println("1. Inventory")
+		fmt.Println("2. Blacksmith")
+		fmt.Println("3. Player Information")
+		fmt.Println("4. Resume the Game")
+		fmt.Println("5. Exit the Game")
 
-	switch key {
-	case 1:
-		key = 1
-		fmt.Println("inventory")
-	case 2:
-		key = 2
-		fmt.Println(" blacksmith ")
-	case 3:
-		key = 3
-		fmt.Print("player information ")
-	case 4:
-		key = 4
-		fmt.Print("resume the game")
-	case 5:
-		key = 5
-		fmt.Print("exit the game ")
-		var confirmation int
-		fmt.Println("Are you sure you want to leave? ?")
-		fmt.Println("1. yes")
-		fmt.Println("2. No")
-		fmt.Print("choice : ")
-		fmt.Scan(&confirmation)
+		var key int
+		fmt.Print("Choice: ")
+		fmt.Scan(&key)
 
-		if confirmation == 1 {
-			fmt.Println("goodbye")
-			os.Exit(0)
-		} else if confirmation == 2 {
-			fmt.Println("back to menu")
-		} else {
-			fmt.Println("invalid choice")
+		switch key {
+		case 1:
+			fmt.Println("Opening inventory")
+		case 2:
+			fmt.Println("Going to the blacksmith")
+		case 3:
+			fmt.Println("Showing player information")
+		case 4:
+			fmt.Println("Resuming the game")
+			return
+		case 5:
+			if ConfirmExit() {
+				fmt.Println("Goodbye ")
+				os.Exit(0)
+			}
+		default:
+			fmt.Println("Invalid choice.")
 		}
 	}
 }
