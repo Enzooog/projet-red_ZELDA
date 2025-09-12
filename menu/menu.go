92ce5239a21c459abecc1106a3dff7a9286ccc45
allan paris <allanparis8@gmail.com>
2025-09-12 14:27:27 +0200

    exit the game

--- menu/menu.go
+++ menu/menu.go
@@ -1,6 +1,9 @@
 package menu
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func menu() {
 	var key int
@@ -22,6 +25,20 @@ func menu() {
 	case 5:
 		key = 5
 		fmt.Print("exit the game ")
+		var confirmation int
+		fmt.Println("Are you sure you want to leave? ?")
+		fmt.Println("1. yes")
+		fmt.Println("2. No")
+		fmt.Print("choice : ")
+		fmt.Scan(&confirmation)
 
+		if confirmation == 1 {
+			fmt.Println("goodbye")
+			os.Exit(0)
+		} else if confirmation == 2 {
+			fmt.Println("back to menu")
+		} else {
+			fmt.Println("invalid choice")
+		}
 	}
 }
