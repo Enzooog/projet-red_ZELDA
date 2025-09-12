bc2d173a94100ab9ad3240c36c1d4df18a403f19
allan paris <allanparis8@gmail.com>
2025-09-12 11:12:51 +0200

    initialise player

--- player.go
+++ player.go
@@ -1,13 +1,12 @@
-package player
+package Player
 
 import (
 	"fmt"
 	"time"
 )
 
-func player() {
+func Player() {
 	fmt.Print(" Welcome to Zelda")
-	time.Sleep(8 * time.Second)
 	time.Sleep(4 * time.Second)
 	fmt.Print(" Choose one breed from these three")
 }
