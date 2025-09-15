313ca47e5f470414cb2c5835409d42bcfb984b38
enzooog <enzo.gruel35@gmail.com>
2025-09-15 14:08:46 +0200

    changes hp depending on class

--- player/player.go
+++ player/player.go
@@ -14,11 +14,9 @@ type Player struct {
 	PvMax  int
 }
 
-// List of available classes
-var validClasses = []string{"Warrior", "Mage", "Archer"}
-
 // Function to check if the class is valid
 func isValidClass(input string) bool {
+	var validClasses = []string{"Warrior", "Mage", "Archer"}
 	for _, class := range validClasses {
 		if strings.EqualFold(input, class) {
 			return true
@@ -31,10 +29,13 @@ func isValidClass(input string) bool {
 func CreatePlayer() Player {
 	var name string
 	var classe string
+	var pv = 100
+
 	fmt.Println("Welcome to Zelda")
 	time.Sleep(1 * time.Second)
 	fmt.Print("Enter your name: ")
 	fmt.Scan(&name)
+
 	for {
 		fmt.Print("Choose your class (Warrior, Mage, Archer): ")
 		fmt.Scan(&classe)
@@ -43,11 +44,20 @@ func CreatePlayer() Player {
 		}
 		fmt.Println("Invalid class. Please choose Warrior, Mage, or Archer.")
 	}
+	if classe == "Warrior" {
+		pv += 20
+	}
+	if classe == "Mage" {
+		pv += 0
+	}
+	if classe == "Archer" {
+		pv -= 20
+	}
 	player := Player{
 		Name:   name,
 		Level:  1,
 		Classe: classe,
-		PvMax:  100,
+		PvMax:  pv,
 	}
 	fmt.Printf("Hello %s, you are a %s.\n", player.Name, player.Classe)
 	fmt.Println("Player level:", player.Level)
