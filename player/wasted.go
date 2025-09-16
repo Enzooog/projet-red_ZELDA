91f9a08ffda640a7382533c4c985c3730c9be74a
enzooog <enzo.gruel35@gmail.com>
2025-09-16 09:31:58 +0200

    function isDead revive after death

--- player/wasted.go
+++ player/wasted.go
@@ -5,7 +5,7 @@ import "fmt"
 func IsDead(p *Player) {
 	if p.Pv <= 0 {
 		p.Pv = p.PvMax / 2
-		fmt.Printf(" %s was dead but has been resurrected with %d HP!\n", p.Name, p.Pv)
+		fmt.Printf("%s was dead but has been resurrected with %d HP!\n", p.Name, p.Pv)
 	} else {
 		fmt.Printf("%s is still alive with %d HP.\n", p.Name, p.Pv)
 	}
