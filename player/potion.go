061fc1817b15eb6c54bb0793a75aa243d1c74d58
enzooog <enzo.gruel35@gmail.com>
2025-09-16 09:02:27 +0200

    function takepot

--- player/potion.go
+++ player/potion.go
@@ -13,5 +13,5 @@ func TakePot(p *Player) {
 	if p.Pv > p.PvMax {
 		p.Pv = p.PvMax
 	}
-	fmt.Printf("You used a potion and recovered %d HP. Current HP: %d/%d\n", heal, p.Pv, p.PvMax)
+	fmt.Printf("You used a potion and recovered %d HP. Current HP : %d/%d\n", heal, p.Pv, p.PvMax)
 }
