43b614a902859369dc41784778ceed8ecedec6ce
allan paris <allanparis8@gmail.com>
2025-09-16 11:13:21 +0200

    maj main

--- main.go
+++ main.go
@@ -1,28 +1,42 @@
 package main
 
-import ("fmt"
-	"projet-red_ZELDA/menu"
-	"projet-red_ZELDA/player"
+import (
+    "fmt"
+    "projet-red_ZELDA/menu"
+    "projet-red_ZELDA/player"
 )
 
 func main() {
-	p := player.CreatePlayer()
-	player.DisplayInfo(p)
+    p := player.CreatePlayer()
+    fmt.Println("\nBienvenue dans l'aventure !")
 
-	p.Pv = 0
-	player.IsDead(&p)
-	player.TakePot(&p)
+    for {
+        fmt.Println("\nQue veux-tu faire ?")
+        fmt.Println("1. Afficher infos joueur")
+        fmt.Println("2. Acheter un objet ")
+        fmt.Println("3. Afficher l'inventaire et équiper un objet")
+        fmt.Println("4. Quitter")
+        fmt.Print("Choix : ")
+        var choix int
+        fmt.Scan(&choix)
 
-	// Achat d’un objet avec l'argent du joueur
-	menu.BuyStone(&p.Money, &p.Inventory)
-	 equipped := p.Inventory.EquipItem()
-    if equipped != nil && equipped.BonusType == "pv" {
-        p.PvMax += equipped.Bonus
-		p.Pv += equipped.Bonus
-        fmt.Printf("Bonus de PV appliqué :%d ! Nouveaux PV max : %d\n",p.Pv, p.PvMax)
+        switch choix {
+        case 1:
+            player.DisplayInfo(p)
+        case 2:
+            menu.BuyStone(&p.Money, &p.Inventory)
+        case 3:
+            equipped := p.Inventory.EquipItem()
+            if equipped != nil && equipped.BonusType == "pv" {
+                p.PvMax += equipped.Bonus
+                p.Pv += equipped.Bonus
+                fmt.Printf("Bonus de PV appliqué : %d ! Nouveaux PV max : %d\n", equipped.Bonus, p.PvMax)
+            }
+        case 4:
+            fmt.Println("Au revoir !")
+            return
+        default:
+            fmt.Println("Choix invalide.")
+        }
     }
-	// Le joueur peut maintenant équiper un objet acheté
-	p.Inventory.EquipItem()
-
-	player.DisplayInfo(p)
 }
