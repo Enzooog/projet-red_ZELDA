7be193a7823681685ab4c5fcee9d0e6d481e3b0a
enzooog <enzo.gruel35@gmail.com>
2025-09-16 11:29:15 +0200

    main

--- main.go
+++ main.go
@@ -13,17 +13,38 @@ func main() {
 	p.Pv = 0
 	player.IsDead(&p)
 	player.TakePot(&p)
+	for {
+		fmt.Println("\nQue veux-tu faire ?")
+		fmt.Println("1. Afficher infos joueur")
+		fmt.Println("2. Acheter un objet ")
+		fmt.Println("3. Afficher l'inventaire et équiper un objet")
+		fmt.Println("4. Quitter")
+		fmt.Print("Choix : ")
+		var choix int
+		fmt.Scan(&choix)
 
-	// Achat d’un objet avec l'argent du joueur
-	menu.BuyStone(&p.Money, &p.Inventory)
-	equipped := p.Inventory.EquipItem()
-	if equipped != nil && equipped.BonusType == "pv" {
-		p.PvMax += equipped.Bonus
-		p.Pv += equipped.Bonus
-		fmt.Printf("Bonus de PV appliqué :%d ! Nouveaux PV max : %d\n", p.Pv, p.PvMax)
+		switch choix {
+		case 1:
+			player.DisplayInfo(p)
+		case 2:
+			menu.BuyStone(&p.Money, &p.Inventory)
+		case 3:
+			equipped := p.Inventory.EquipItem()
+			if equipped != nil && equipped.BonusType == "pv" {
+				p.PvMax += equipped.Bonus
+				p.Pv += equipped.Bonus
+				fmt.Printf("Bonus de PV appliqué : %d ! Nouveaux PV max : %d\n", equipped.Bonus, p.PvMax)
+			}
+		case 4:
+			fmt.Println("Au revoir !")
+			return
+		default:
+			fmt.Println("Choix invalide.")
+		}
+
+		// Le joueur peut maintenant équiper un objet acheté
+		p.Inventory.EquipItem()
+
+		player.DisplayInfo(p)
 	}
-	// Le joueur peut maintenant équiper un objet acheté
-	p.Inventory.EquipItem()
-
-	player.DisplayInfo(p)
 }
